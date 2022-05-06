9ab5acb1e053c2b2a34c760cfa42c2854aa4490d
Alex <alex@imaleex.com>
2022-05-06 12:52:23 +0200

    Upload

--- Fase 3/src/src/com/imaleex/esportapp/Models/Equipo.java
+++ Fase 3/src/src/com/imaleex/esportapp/Models/Equipo.java
@@ -4,4 +4,31 @@ package com.imaleex.esportapp.Models;
  * @author Alex Cortes
  */
 public class Equipo {
+    private int id;
+    private String nombre;
+
+    public Equipo() {
+    }
+
+    public Equipo(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
