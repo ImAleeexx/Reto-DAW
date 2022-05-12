9f1546402d76d6261952f58122f88e95998dddf1
Alex <alex@imaleex.com>
2022-05-12 11:46:00 +0200

    Upload backup

--- Codigo Fuente/src/src/com/imaleex/esportapp/Main.java
+++ Codigo Fuente/src/src/com/imaleex/esportapp/Main.java
@@ -1,11 +1,25 @@
 package com.imaleex.esportapp;
 
+import com.imaleex.esportapp.Db.Db;
+import com.imaleex.esportapp.Exceptions.DbException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    public static Db db;
 
+    public static void main(String[] args) {
+        initDbConnection();
+
+    }
+    private static void initDbConnection() {
+        try {
+        Db.setDbParams("esports", "ijG4ZHcn81QlxfOA", "esports", "server.imaleex.com");
+        db = Db.getInstance(1);
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
