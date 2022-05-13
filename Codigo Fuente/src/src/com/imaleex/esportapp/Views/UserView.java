1f0e2a8d24ec82bef6e7a80819b048821876df84
Aritz Castillo <aritzcas@gmail.com>
2022-05-13 09:51:38 +0200

    View Usuario

---
+++ Codigo Fuente/src/src/com/imaleex/esportapp/Views/UserView.java
@@ -0,0 +1,9 @@
+package com.imaleex.esportapp.Views;
+
+import javax.swing.*;
+
+public class UserView {
+    private JTextField tfUserName;
+    private JMenu jmUltimaJornada;
+    private JMenu jmClasificacion;
+}
