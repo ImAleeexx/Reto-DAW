fa3687c660ce040f6fcfdd9d163046df998b1b05
Alex <alexcortes90@gmail.com>
2022-05-18 04:17:21 +0200

    Algoritmo generacion de partidos completo

--- Codigo Fuente/src/src/com/imaleex/esportapp/Controllers/AdminController.java
+++ Codigo Fuente/src/src/com/imaleex/esportapp/Controllers/AdminController.java
@@ -11,8 +11,10 @@ import com.imaleex.esportapp.Main;
 import com.imaleex.esportapp.Models.Equipo;
 import com.imaleex.esportapp.Models.Personas.Dueno;
 import com.imaleex.esportapp.Models.Personas.Entrenador;
-import com.imaleex.esportapp.Models.Personas.Jugador;
 import com.imaleex.esportapp.Models.Users.Usuario;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * @author Alex Cortes
@@ -57,5 +59,55 @@ public class AdminController {
             throw new RuntimeException(e);
         }
     }
+
+
+    public static void generateMatches(){
+
+        ArrayList<String> listaEquipos = new ArrayList<>();
+        ArrayList<String> listaEquipos1 = new ArrayList<>();
+        ArrayList<String> listaEquipos2 = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            listaEquipos.add("Equipo" + i);
+        }
+
+        Collections.shuffle(listaEquipos);
+
+        for (int i = 0; i < listaEquipos.size(); i++) {
+            if (i <= 4) {
+                listaEquipos1.add(listaEquipos.get(i));
+            } else {
+                listaEquipos2.add(listaEquipos.get(i));
+            }
+        }
+        //Reverseamos la lista 2
+        Collections.reverse(listaEquipos2);
+        int numJornadas = (listaEquipos.size() -1);
+        int numPartidos = listaEquipos.size() / 2;
+        StringBuilder sb = new StringBuilder();
+        //Loop por cada jornada
+        for (int i = 0; i < numJornadas; i++) {
+            sb.append("Jornada " + (i + 1) + "\n");
+            //Loop por cada partido de la jornada
+            for (int j = 0; j < numPartidos; j++) {
+                String equipo1 = listaEquipos1.get(j);
+                String equipo2 = listaEquipos2.get(j);
+                sb.append(equipo1 + " vs " + equipo2 + "\n");
+            }
+            sb.append("\n");
+            String equipoMoverLista1 = listaEquipos1.get(listaEquipos1.size() - 1);
+            String equipoMoverLista2 = listaEquipos2.get(0);
+
+            //Rotamos con orientacion de agujas del reloj
+            listaEquipos1.remove(listaEquipos1.size() - 1);
+            listaEquipos2.add(equipoMoverLista1);
+
+            listaEquipos2.remove(0);
+            listaEquipos1.add(1, equipoMoverLista2);
+        }
+        System.out.println(sb.toString());
+        //Todo make vuelta partidos
+    }
+
+
 
 }
--- Codigo Fuente/src/src/com/imaleex/esportapp/Main.java
+++ Codigo Fuente/src/src/com/imaleex/esportapp/Main.java
@@ -1,46 +1,39 @@
 package com.imaleex.esportapp;
 
-import com.imaleex.esportapp.Db.Dao.Personas.DuenoDAO;
-import com.imaleex.esportapp.Db.Dao.UserDAO;
+import com.imaleex.esportapp.Controllers.AdminController;
 import com.imaleex.esportapp.Db.Db;
-import com.imaleex.esportapp.Exceptions.DataNotFoundException;
 import com.imaleex.esportapp.Exceptions.DbException;
 import com.imaleex.esportapp.Exceptions.UserNotFoundException;
-import com.imaleex.esportapp.Models.Personas.Dueno;
 import com.imaleex.esportapp.Models.Users.Usuario;
 import com.imaleex.esportapp.Utils.CryptoUtils;
-import com.imaleex.esportapp.Utils.WindowUtils;
 import com.imaleex.esportapp.Views.AdminView;
 import com.imaleex.esportapp.Views.Login;
 import com.imaleex.esportapp.Views.UserView;
-import sun.misc.MessageUtils;
 
 import javax.swing.*;
-
-import java.io.BufferedReader;
 import java.io.Console;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 import static com.imaleex.esportapp.Db.Dao.UserDAO.searchUsername;
 
 public class Main {
 
     public static Db db;
+    public static Usuario user;
     private static JFrame login;
     private static JFrame adminV;
     private static JFrame userV;
-    public static  Usuario user;
 
     public static void main(String[] args) {
         initDbConnection();
         displayLoginModal();
+        AdminController.generateMatches();
     }
+
     private static void initDbConnection() {
         try {
             //Get password from enviroment variables
             String password = System.getenv("DB_PASSWORD");
-            if (password == null ) {
+            if (password == null) {
                 throw new DbException("No se encontró la contraseña de la base de datos en la variable de entorno DB_PASSWORD");
             }
             Db.setDbParams("esports", password, "esports", "server.imaleex.com");
@@ -65,29 +58,30 @@ public class Main {
     /* Login */
     public static boolean login(String username, String password) {
         Usuario usuario = null;
-            try {
-                usuario = searchUsername(username);
-                if (usuario != null) {
-                    if (CryptoUtils.checkHash(password, usuario.getClave())) {
-                        user = usuario;
-                        return true;
-                    }
-                } else {
-                    throw new UserNotFoundException("El usuario no existe");
+        try {
+            usuario = searchUsername(username);
+            if (usuario != null) {
+                if (CryptoUtils.checkHash(password, usuario.getClave())) {
+                    user = usuario;
+                    return true;
                 }
-            } catch (UserNotFoundException ignored) {
+            } else {
+                throw new UserNotFoundException("El usuario no existe");
             }
+        } catch (UserNotFoundException ignored) {
+        }
         return false;
     }
-    public static void mostrarVentana(){
-        if (user.checkAdmin()){
+
+    public static void mostrarVentana() {
+        if (user.checkAdmin()) {
             AdminView.main();
-        }else{
+        } else {
             UserView.main();
         }
     }
 
-    private static void displayLoginModal(){
+    private static void displayLoginModal() {
         login = new JFrame("Login");
         login.setContentPane(new Login().getJPanel());
         login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -95,7 +89,8 @@ public class Main {
         login.pack();
         login.setVisible(true);
     }
-    public static void closeLogin(){
+
+    public static void closeLogin() {
         login.dispose();
     }
 
